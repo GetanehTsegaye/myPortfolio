355b732d5b927aa3ea8fa64fe1e0c40e55e15b08
Getaneh Tsegaye <getanehtsegaye@gmail.com>
2024-06-12 18:36:38 -0400

    I have Installed Tailwind, created a folder structure with empty components, sections and constants

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-
+import { About, Contact, Header, Home, Portfolio } from "./sections";
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+      <About />
+      <Portfolio />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
